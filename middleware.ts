0f2e7a4db1ed048f871606fffeeb1b52c015a584
Alejandro Vilaca <alejandro.vilaca@itti.digital>
2024-03-05 17:50:43 -0300

    Revert "trying to fix run build"
    This reverts commit 8542bf44276f28a8fa3f47869cdc640a5e225336.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
-/* import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+// Maybe add prisma check for authorizarion maybe ?
 
 export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get("next-auth.session-token");
 
   if (sessionToken) {
+    //console.log("isAuthenticated");
     return NextResponse.next();
   }
 
+  // TODO: uncomment real path
   return NextResponse.redirect(new URL("/", request.url));
   //return NextResponse.next();
 }
@@ -14,4 +17,3 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/lists/:path*"],
 };
- */
