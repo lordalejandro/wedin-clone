8542bf44276f28a8fa3f47869cdc640a5e225336
Alejandro Vilaca <alejandro.vilaca@itti.digital>
2024-03-05 17:47:12 -0300

    trying to fix run build

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
-import { NextResponse, NextRequest } from "next/server";
-// Maybe add prisma check for authorizarion maybe ?
+/* import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get("next-auth.session-token");
 
   if (sessionToken) {
-    //console.log("isAuthenticated");
     return NextResponse.next();
   }
 
-  // TODO: uncomment real path
   return NextResponse.redirect(new URL("/", request.url));
   //return NextResponse.next();
 }
@@ -17,3 +14,4 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/lists/:path*"],
 };
+ */
